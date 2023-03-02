93d4ba1fc7ac6cf3c3e127df8551769c2b940bdd
aniketparmar29 <aniketparmar068@gmail.com>
2023-03-02 20:41:10 +0530

    a

--- main/src/App.tsx
+++ main/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Mainroutes from './pages/mainroutes';
+import Mainroutes from './pages/Mainroutes';
 function App() {
   return (
     <>
--- main/src/index.tsx
+++ main/src/index.tsx
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import {BrowserRouter} from 'react-router-dom'
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
+  <BrowserRouter>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
