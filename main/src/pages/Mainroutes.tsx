c812992238d30b376c0ef62dbe65482709f418a4
aniketparmar29 <aniketparmar068@gmail.com>
2023-03-03 20:47:01 +0530

    cricket

--- main/src/pages/Mainroutes.tsx
+++ main/src/pages/Mainroutes.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import {Routes,Route} from 'react-router-dom';
 import Home from './Home';
+import Aniket from '../aniket/Aniket';
 function Mainroutes() {
   return (
     <>
     <Routes>
         <Route path='/' element={<Home/>}></Route>
+        <Route path='/cricket' element={<Aniket/>}></Route>
     </Routes>
     </>
   )
