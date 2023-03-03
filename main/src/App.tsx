df30c58cbd433d47d97e02075c811ca519ca770b
mohitd427 <mohitdon0013@gmail.com>
2023-03-03 18:52:50 +0530

    added word guess game

--- main/src/App.tsx
+++ main/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Mainroutes from './pages/Mainroutes';
+import WordGuess from './pages/WordGuess';
 function App() {
   return (
     <>
-    <Mainroutes/>
+      <Mainroutes />
+      
     </>
   );
 }
