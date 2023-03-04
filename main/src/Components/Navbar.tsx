8284182a173482d7344f6bd035cee877c71f33c6
mohitd427 <mohitdon0013@gmail.com>
2023-03-04 22:37:52 +0530

    added navbar reg login

--- main/src/Components/Navbar.tsx
+++ main/src/Components/Navbar.tsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import {Box,Grid,GridItem} from '@chakra-ui/react'
 
-function Navbar() {
+const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Grid templateColumns='repeat(5, 1fr)' gap={4} bg='black'>
+      <GridItem colSpan={1} h='40'
+        color='tomato'
+        alignItems={'center'} display='flex' justifyContent={'center'} >Logo</GridItem>
+      <GridItem colSpan={1} h='40' color='tomato'
+        alignItems={'center'}
+        display='flex' justifyContent={'center'} >
+        Games
+      </GridItem>
+      <GridItem colSpan={1} h='40' color='tomato'
+        alignItems={'center'}
+        display='flex' justifyContent={'center'} >
+        Leaderboard
+      </GridItem>
+      <GridItem
+        colStart={4}
+        colEnd={6} h='40' color='papayawhip' alignItems={'center'} display='flex'
+        justifyContent={'center'}>
+        Login/SignUp
+      </GridItem>
+    </Grid>
   )
 }
+
+
+
+
 
 export default Navbar
--- main/src/pages/Mainroutes.tsx
+++ main/src/pages/Mainroutes.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import {Routes,Route} from 'react-router-dom';
 import Game from '../aniket/Game';
+import Register from './Register';
 import Snake from '../snakegame/Snake';
 import Home from './Home';
 import PageNotFound from './PageNotFOund';
 import WordGuess from './WordGuess';
+import Login from './Login';
 function Mainroutes() {
   return (
     <>
@@ -12,7 +14,9 @@ function Mainroutes() {
         <Route path='/' element={<Home/>}></Route>
         <Route path='/wordguess' element={<WordGuess />}></Route>      
         <Route path='/shoot' element={<Game />}></Route> 
-        <Route path='/snake' element={<Snake/>}></Route>     
+        <Route path='/snake' element={<Snake />}></Route>
+        <Route path='/register' element={<Register />}></Route>
+        <Route path='/login' element={<Login />}></Route>     
         <Route path='*' element={<PageNotFound/>}></Route>     
     </Routes>
     </>
